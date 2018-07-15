b7d83296a80a6cbaa88156a3b5f5255d44136672
miki1989 <mikolajheller@gmail.com>
2018-07-15 17:47:20 +0200

    Java-16 with reverse print of random 10 numbers. Updated version with one for loop for numbers.

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,32 @@
 import java.util.*;
-import java.util.Arrays;
 import java.util.stream.IntStream;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        scanner.useLocale(Locale.US);
+
 
         Random random = new Random();
         int [] tablica = new int[10];
-        String [] tab;
-        int x = 9;
-        String arr;
+        // Sposob drugi to klonowanie z uzyciem petli while , sa to fragmenty zakomentowane int x = 9;
+
 
         for (int i = 0; i < tablica.length; i++) {
                 tablica[i] = random.nextInt(100);
-                System.out.print(tablica[i] + " ");
+                //System.out.print(tablica[i] + " ");
+        }
+        for(int j = 0; j < tablica.length*2; j++){
+            if(j < tablica.length){
+                System.out.print(tablica[j] + " ");
+            }
+            else{
+                System.out.print(tablica[(2*tablica.length - 1)-j] +" ");
+            }
         }
 
 
-        int[] tab2 = tablica.clone();
+        /*int[] tab2 = tablica.clone();
 
         while (x < tab2.length && x >=0 ){
             System.out.print(tab2[x] + " ");
@@ -32,5 +37,6 @@ public class Main {
         //    System.out.print(tab2[x]+ " ");
         //    x++;
         //}
+        */
     }
 }
